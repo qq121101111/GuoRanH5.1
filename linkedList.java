39f2af7eb7cbca2af79439ea9c616d8e0056fcf5
Ran Guo <rguo2@albany.edu>
2017-04-16 22:08:40 -0400

    default constructor,method getSize(), isEmpty()

--- linkedList.java
+++ linkedList.java
@@ -3,6 +3,20 @@ public class linkedList{
   private Node head = null;
   private int size = 0;
   
+  //default constructor
+  public linkedList(){
+    head = null;
+  }
+  
+  //method getSize
+  public int getSize(){
+    return this.size;
+  }
+  
+  //method isEmpty()
+  public boolean isEmpty(){
+    return head == null;
+  }
   
 
 
