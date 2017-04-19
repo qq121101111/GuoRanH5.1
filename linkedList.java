6d180daee629a290288f2546c64831fab5e77148
Ran Guo <rguo2@albany.edu>
2017-04-18 22:53:13 -0400

    add methof populateList() and debug

--- linkedList.java
+++ linkedList.java
@@ -1,4 +1,7 @@
+import java.util.Random;
+
 public class linkedList{
+  
   //properties
   private Node head = null;
   private int size = 0;
@@ -7,6 +10,7 @@ public class linkedList{
   public linkedList(){
     head = null;
   }
+  
   
   //method getSize
   public int getSize(){
@@ -19,21 +23,28 @@ public class linkedList{
   }
   
   //method addNumber() add a node to the end of list
-  public viod addNumber(Node node){
+  public void addNumber(Node node){
     Node newNode = new Node(node.getNumber()); 
     if(isEmpty() == true){      //  if list is empty then add the node to head 
       head = newNode;
     }
     else{                       
       Node list = head;
-      while(list.getNext()! = null){   // loop add node
-        list = list.getNext;
+      while(list.getNext() != null){   // loop add node
+        list = list.getNext();
       }
       list.setNext(newNode);
     }
     size++;
   }
-
+   //method populate 
+  public void populateList(int length){
+    Random random = new Random();
+    for(int i = 0;i < length;i++){
+      int n = random.nextInt(100);
+      addNumber(new Node(n));
+    }
+   }
   
   
   
