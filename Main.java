6f8263ad94a18f0796a3672ebb8432a5e2f1eac3
Ran Guo <rguo2@albany.edu>
2017-04-23 00:47:15 -0400

    debug

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main{
 
     public static void main(String[] args) { 
-    LinkedList list1 = new LinkedList();
+    linkedList list1 = new linkedList();
     
     list1.populateList(10);
     
