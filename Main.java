bee8e4de087e788937333ef2d4c77e21fad3a66b
Ran Guo <rguo2@albany.edu>
2017-04-23 00:45:41 -0400

    testing

--- Main.java
+++ Main.java
@@ -1,4 +1,18 @@
 public class Main{
 
+    public static void main(String[] args) { 
+    LinkedList list1 = new LinkedList();
+    
+    list1.populateList(10);
+    
+    System.out.println("unsort");
+    list1.print();
+    System.out.println("after");
+    list1.bubbleSort();
+    list1.print();
+    
+    
+  
+  }
 
 }
